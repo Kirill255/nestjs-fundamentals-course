3663868ea2abeacfbf65ee0f6417deebfcd21882
Kirill <2418447-Kirill255@users.noreply.gitlab.com>
2022-02-13 02:39:08 +0300

    add coffees service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -11,9 +11,122 @@ import {
   Query,
   Res,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  /**
+   * GET http://localhost:3000/coffees?offset=10&limit=20
+   * @param query
+   * @returns
+   */
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getAllCoffees(@Query() query) {
+    const { offset, limit } = query;
+    const allCoffees = await this.coffeesService.getAllCoffees(offset, limit);
+    return {
+      handler: 'getAllCoffees',
+      data: allCoffees,
+      offset,
+      limit,
+    };
+  }
+
+  /**
+   * GET http://localhost:3000/coffees/123
+   * @param id
+   * @returns
+   */
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async getOneCoffee(@Param('id') id: string) {
+    const coffee = await this.coffeesService.getOneCoffee(id);
+    return {
+      handler: 'getOneCoffee',
+      data: coffee,
+      id,
+    };
+  }
+
+  /**
+   * POST http://localhost:3000/coffees
+   * @param body
+   * @returns
+   * {
+   *  "id": 2,
+   *  "name": "MacCoffee Latte Al Caramello",
+   *  "brand": "MacCoffe",
+   *  "flavor": ["caramel"],
+   * }
+   */
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async createCoffee(@Body() body) {
+    const newCoffee = await this.coffeesService.createCoffee(body);
+    return {
+      handler: 'createCoffee',
+      data: newCoffee,
+      body,
+    };
+  }
+
+  /**
+   * PATCH http://localhost:3000/coffees/123
+   * @param id
+   * @param body
+   * @returns
+   * {
+   *  "name": "Espresso"
+   * }
+   */
+  @Patch(':id')
+  @HttpCode(HttpStatus.OK)
+  async updateCoffee(@Param('id') id: string, @Body() body) {
+    const updatedCoffee = await this.coffeesService.updateCoffee(id, body);
+    return {
+      handler: 'updateCoffee',
+      data: updatedCoffee,
+      id,
+      body,
+    };
+  }
+
+  /**
+   * DELETE http://localhost:3000/coffees/123
+   * @param id
+   * @returns
+   */
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  async removeCoffee(@Param('id') id: string) {
+    const removedCoffee = await this.coffeesService.removeCoffee(id);
+    return {
+      handler: 'removeCoffee',
+      data: removedCoffee,
+      id,
+    };
+  }
+
+  /* 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  */
+
   /**
    * GET http://localhost:3000/coffees
    * @returns
@@ -69,14 +182,14 @@ export class CoffeesController {
    * @param query
    * @returns
    */
-  @Get()
-  getAllCoffees(@Query() query) {
-    const { offset, limit } = query;
-    return {
-      handler: 'getAllCoffees',
-      data: { offset, limit },
-    };
-  }
+  // @Get()
+  // getAllCoffees(@Query() query) {
+  //   const { offset, limit } = query;
+  //   return {
+  //     handler: 'getAllCoffees',
+  //     data: { offset, limit },
+  //   };
+  // }
 
   /**
    * GET http://localhost:3000/coffees/123
@@ -96,13 +209,13 @@ export class CoffeesController {
    * @param id
    * @returns
    */
-  @Get(':id')
-  getOneCoffee(@Param('id') id: string) {
-    return {
-      handler: 'getOneCoffee',
-      data: { id },
-    };
-  }
+  // @Get(':id')
+  // getOneCoffee(@Param('id') id: string) {
+  //   return {
+  //     handler: 'getOneCoffee',
+  //     data: { id },
+  //   };
+  // }
 
   /**
    * POST http://localhost:3000/coffees
@@ -130,14 +243,14 @@ export class CoffeesController {
    *  "name": "MacCoffe"
    * }
    */
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  createCoffee(@Body('name') name: string) {
-    return {
-      handler: 'createCoffee',
-      data: { name },
-    };
-  }
+  // @Post()
+  // @HttpCode(HttpStatus.CREATED)
+  // createCoffee(@Body('name') name: string) {
+  //   return {
+  //     handler: 'createCoffee',
+  //     data: { name },
+  //   };
+  // }
 
   /**
    * PATCH http://localhost:3000/coffees/123
@@ -148,38 +261,38 @@ export class CoffeesController {
    *  "name": "Espresso"
    * }
    */
-  @Patch(':id')
-  @HttpCode(HttpStatus.NO_CONTENT) // 204 успех, можно и 200 возвращать
-  updateCoffee(@Param('id') id: string, @Body() body) {
-    return {
-      handler: 'updateCoffee',
-      data: { id, body },
-    };
-  }
+  // @Patch(':id')
+  // @HttpCode(HttpStatus.NO_CONTENT) // 204 успех, можно и 200 возвращать
+  // updateCoffee(@Param('id') id: string, @Body() body) {
+  //   return {
+  //     handler: 'updateCoffee',
+  //     data: { id, body },
+  //   };
+  // }
 
   /**
    * DELETE http://localhost:3000/coffees/123
    * @param id
    * @returns
    */
-  @Delete(':id')
-  @HttpCode(HttpStatus.OK)
-  removeCoffee(@Param('id') id: string) {
-    return {
-      handler: 'removeCoffee',
-      data: { id },
-    };
-  }
+  // @Delete(':id')
+  // @HttpCode(HttpStatus.OK)
+  // removeCoffee(@Param('id') id: string) {
+  //   return {
+  //     handler: 'removeCoffee',
+  //     data: { id },
+  //   };
+  // }
 
   /**
    * GET http://localhost:3000/coffees/top
    * @returns
    */
-  @Get('top')
-  getTopCoffees() {
-    return {
-      handler: 'getTopCoffees',
-      data: {},
-    };
-  }
+  // @Get('top')
+  // getTopCoffees() {
+  //   return {
+  //     handler: 'getTopCoffees',
+  //     data: {},
+  //   };
+  // }
 }
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+  async getAllCoffees(offset: string | number, limit: string | number) {
+    return this.coffees;
+  }
+
+  async getOneCoffee(id: string) {
+    const coffee = this.coffees.find((c) => c.id === Number(id));
+    return coffee || {};
+  }
+
+  async createCoffee(body: any) {
+    this.coffees.push(body);
+    return body;
+  }
+
+  async updateCoffee(id: string, body: any) {
+    const coffee = await this.getOneCoffee(id);
+    if (!coffee) return {};
+    const updatedCoffee = Object.assign({}, coffee, body);
+    return updatedCoffee;
+  }
+
+  async removeCoffee(id: string) {
+    const coffeeIndex = this.coffees.findIndex((c) => c.id === Number(id));
+    if (coffeeIndex === -1) return {};
+    const removedCoffee = this.coffees.splice(coffeeIndex, 1);
+    return removedCoffee;
+  }
+}

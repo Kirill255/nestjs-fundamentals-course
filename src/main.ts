720d53440b182eb5053e13d8b9205cc4fcad65f7
Kirill <2418447-Kirill255@users.noreply.gitlab.com>
2022-02-13 17:54:16 +0300

    add whitelist and forbidNonWhitelisted to ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // если с фронта передаются поля невключенные в описание типа, они не попадут в условный req.body(например, createCoffeeDto) в контроллере
+      forbidNonWhitelisted: true, // если с фронта передаются поля невключенные в описание типа будет выбрасываться ошибка
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();

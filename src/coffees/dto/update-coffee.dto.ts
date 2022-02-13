c05e97f2f9b70c467eba4f7b04c21b9cb0c1b48f
Kirill <2418447-Kirill255@users.noreply.gitlab.com>
2022-02-13 17:45:19 +0300

    validate input data with dto

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,18 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
+
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
+
+/* 
+для UpdateCoffeeDto наследуем описание типов из CreateCoffeeDto, но при этом при помощи PartialType, делаем все поля необязательными
+Это выглядело бы примерно вот так
+
 export class UpdateCoffeeDto {
+  @IsString()
   readonly name?: string;
+  @IsString()
   readonly brand?: string;
+  @IsString({ each: true })
   readonly flavors?: string[];
 }
+*/

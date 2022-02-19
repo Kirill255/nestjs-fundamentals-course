0ed1c993d1f4895138378c1ba083facb1cffbf65
Kirill <24504648+Kirill255@users.noreply.github.com>
2022-02-20 00:25:11 +0300

    use repository to access db

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -4,24 +4,32 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: 'Shipwreck Roast',
-      brand: 'Buddy Brew',
-      flavors: ['chocolate', 'vanilla'],
-    },
-  ];
+  // private coffees: Coffee[] = [
+  //   {
+  //     id: 1,
+  //     name: 'Shipwreck Roast',
+  //     brand: 'Buddy Brew',
+  //     flavors: ['chocolate', 'vanilla'],
+  //   },
+  // ];
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
   async getAllCoffees(offset: string | number, limit: string | number) {
-    return this.coffees;
+    return this.coffeeRepository.find();
   }
 
   async getOneCoffee(id: string) {
-    const coffee = this.coffees.find((c) => c.id === Number(id));
+    const coffee = await this.coffeeRepository.findOne(Number(id));
     if (!coffee) {
       // throw new HttpException(
       //   `Coffee with id ${id} not found`,
@@ -32,22 +40,28 @@ export class CoffeesService {
     return coffee;
   }
 
-  async createCoffee(createCoffeeDto: any) {
-    this.coffees.push(createCoffeeDto);
-    return createCoffeeDto;
+  async createCoffee(createCoffeeDto: CreateCoffeeDto) {
+    // тут пока не очень понял, сначала создаётся инстанс согласно описанию в модели, а потом сущность сохраняется в базу, возможно есть способ добавть в базу новую строку одним действием
+    // TODO: тут нужен await?
+    const coffee = this.coffeeRepository.create(createCoffeeDto);
+    return this.coffeeRepository.save(coffee);
   }
 
-  async updateCoffee(id: string, updateCoffeeDto: any) {
-    const coffee = await this.getOneCoffee(id);
-    if (!coffee) return {};
-    const updatedCoffee = Object.assign({}, coffee, updateCoffeeDto);
-    return updatedCoffee;
+  async updateCoffee(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    // блять тут вообще не понял теперь, если в create методе мы типа создавали инстанс модели и потом его сохраняли в базу(save), то зачем тут save вызывается?
+    // метод preload находит в базе сущность и если находит заменяет/обновляет все переданные свойства, если не находит, то возвращает undefined, зачем после save вызывается?
+    const coffee = await this.coffeeRepository.preload({
+      id: Number(id),
+      ...updateCoffeeDto,
+    });
+    if (!coffee) {
+      throw new NotFoundException(`Coffee with id ${id} not found`);
+    }
+    return this.coffeeRepository.save(coffee);
   }
 
   async removeCoffee(id: string) {
-    const coffeeIndex = this.coffees.findIndex((c) => c.id === Number(id));
-    if (coffeeIndex === -1) return {};
-    const removedCoffee = this.coffees.splice(coffeeIndex, 1);
-    return removedCoffee;
+    const coffee = await this.getOneCoffee(id);
+    return this.coffeeRepository.remove(coffee);
   }
 }

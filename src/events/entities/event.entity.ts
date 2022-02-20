c7c9e03fd56dd8d8b38e016f08131187bc39967e
Kirill <24504648+Kirill255@users.noreply.github.com>
2022-02-20 23:54:41 +0300

    use indices

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,13 +1,16 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
+@Index(['name', 'type']) // составной индекс
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
   id: number;
 
+  // @Index() // добавление в индекс конкретного столбца
   @Column()
   type: string;
 
+  // @Index('name-idx') // добавление в индекс конкретного столбца с указанием названия индекса
   @Column()
   name: string;
 

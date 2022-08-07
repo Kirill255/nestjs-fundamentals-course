5263eeb71658757f63607b517c3952a14cf2dc1f
Kirill <24504648+Kirill255@users.noreply.github.com>
2022-08-07 23:49:28 +0300

    class provider

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -9,6 +9,10 @@ import { COFFEE_BRANDS } from './coffees.constants';
 
 // class MockCoffeesService {}
 
+class ConfigService {}
+class DevConfigService {}
+class ProdConfigService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
@@ -28,6 +32,13 @@ import { COFFEE_BRANDS } from './coffees.constants';
   providers: [
     CoffeesService,
     { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }, // провайдер, не основанный на классах, в данном примере мы используем строку 'COFFEE_BRANDS', чтобы заинжектить такой провайдер нужно использовать декоратор @Inject(), пример @Inject('COFFEE_BRANDS') coffeeBrands: string[],
+    {
+      provide: ConfigService,
+      useValue:
+        process.env.NODE_ENV === 'production'
+          ? ProdConfigService
+          : DevConfigService,
+    },
   ],
   exports: [CoffeesService],
 })
